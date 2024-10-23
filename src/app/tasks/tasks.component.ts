4a977bed20ec4c0ff957ff87a67a7156490a8e1a
Agahuseyn İsmayılov <aga@Agahuseyn-MacBook-Air.local>
2024-10-23 22:53:09 +0400

    İlk commit

---
+++ src/app/tasks/tasks.component.ts
@@ -0,0 +1,69 @@
+import { Component, Input } from '@angular/core';
+import { TaskComponent } from "./task/task.component";
+import { NewTaskComponent } from "./new-task/new-task.component";
+import { type NewTaskData } from './task/task.model';
+
+@Component({
+  selector: 'app-tasks',
+  standalone: true,
+  imports: [TaskComponent, NewTaskComponent],
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.css'
+})
+export class TasksComponent {
+  @Input({required: true}) userId!: string;
+  @Input({required: true}) name!: string;
+  isAddingTask = false;
+  tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Master Angular',
+      summary: 'Learn all the basic and advanced features of Angular & how to apply them',
+      dueDate: '2025-12-31'
+    },
+    {
+      id: 't2',
+      userId: 'u1',
+      title: 'Build first prototype',
+      summary: 'Learn all the basic and advanced features of Angular & how to apply them',
+      dueDate: '2025-12-31'
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'Prepare issue template',
+      summary: 'Learn all the basic and advanced features of Angular & how to apply them',
+      dueDate: '2025-12-31'
+    },
+  ];
+
+  get selectedUserTasks() {
+    return this.tasks.filter((task) => task.userId === this.userId);
+  }
+
+  onCompleteTask(id: string) {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
+  }
+
+  onCancelAddTask() {
+    this.isAddingTask = false;
+  }
+
+  onAddTask(taskData: NewTaskData) {
+    this.tasks.unshift({
+      id: new Date().getDate().toString(),
+      userId: this.userId,
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate: taskData.date
+    })
+    this.isAddingTask = false;
+
+}
+
+}
